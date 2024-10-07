f8450a919ea3cff10928ac197fa071aa0579ead4
Fro5tyDog <163802544+Fro5tyDog@users.noreply.github.com>
2024-10-07 14:23:28 +0800

    Camera Facing

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ function renderPlaces(places) {
         const icon = document.createElement('a-entity');
         icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);  // Sets the location
         icon.setAttribute('gltf-model', "#cube-asset");  // Sets the 3D model to be loaded (defined in index.html)
+        icon.setAttribute('look-at', '[gps-camera]'); // Ensures object always faces you. 
         icon.setAttribute('scale', '15 15 15');  // Sets the scale of the 3D model so it is visible
 
         // This part dispatches an event once the GPS-based entity is loaded (optional for debugging)
@@ -42,38 +43,3 @@ function renderPlaces(places) {
         scene.appendChild(icon);  // Adds the 3D model to the A-frame scene
     });
 }
-
-
-    // function renderPlaces(places) {
-    //     let scene = document.querySelector('a-scene');
-    //     places.forEach((place) => {
-    //         const latitude = place.location.lat;
-    //         const longitude = place.location.lng;
-    //         // add place icon
-    //         const icon = document.createElement('a-entity');
-    //         icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-    //         icon.setAttribute('gltf-model', "#cube-asset");
-    //         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-    //         icon.setAttribute('scale', '15 15 15');
-    //         icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-    //         const clickListener = function (ev) {
-    //            ev.stopPropagation();
-    //             ev.preventDefault();
-    //             console.log('Clicked');
-    //             const name = ev.target.getAttribute('name');
-    //             const el = ev.detail.intersection && ev.detail.intersection.object.el;
-    //             if (el && el === ev.target) {
-    //                 const label = document.createElement('span');
-    //                 const container = document.createElement('div');
-    //                 container.setAttribute('id', 'place-label');
-    //                 label.innerText = name;
-    //                 container.appendChild(label);
-    //                 document.body.appendChild(container);
-    //                 setTimeout(() => {
-    //                     container.parentElement.removeChild(container);
-    //                 }, 1500);
-    //             }
-    //         };
-    //         icon.addEventListener('click', clickListener);
-    //         scene.appendChild(icon);
-    //     });
