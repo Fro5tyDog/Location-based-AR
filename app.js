a48fcd0f4b5e5ddea116f9881761ff173a4d8641
Fro5tyDog <163802544+Fro5tyDog@users.noreply.github.com>
2024-10-07 12:27:20 +0800

    cube-asset

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
-fetch('config.json')
-  .then(response => response.json())
-  .then(config => {
-    const scene = document.querySelector('a-scene');
-    const location = config.location;  // Only handling one location for now
+window.onload = () => {
+    let places = staticLoadPlaces();
+    return renderPlaces(places);
+};
 
-    // Dynamically add the 3D model based on the configuration
-    if (location['3dModel']) {
-      const modelEntity = document.createElement('a-entity');
-      modelEntity.setAttribute('gltf-model', `src: ${location['3dModel']}`);
-      modelEntity.setAttribute('gps-entity-place', `latitude: ${location.latitude}; longitude: ${location.longitude};`);
-      modelEntity.setAttribute('scale', '100 100 100');
-      scene.appendChild(modelEntity);
-    }
-  });
+function staticLoadPlaces() {
+    return [
+        {
+            name: "LTA",
+            location: {
+                lat: 1.30877,
+                lng: 103.84863
+            }
+        }
+    ];
+}
+
+    function renderPlaces(places) {
+        let scene = document.querySelector('a-scene');
+        places.forEach((place) => {
+            const latitude = place.location.lat;
+            const longitude = place.location.lng;
+            // add place icon
+            const icon = document.createElement('a-entity');
+            icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+            icon.setAttribute('gltf-model', "#cube-asset");
+            // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+            icon.setAttribute('scale', '0.21 0.21 0.21');
+            icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+            const clickListener = function (ev) {
+               ev.stopPropagation();
+                ev.preventDefault();
+                console.log('Clicked');
+                const name = ev.target.getAttribute('name');
+                const el = ev.detail.intersection && ev.detail.intersection.object.el;
+                if (el && el === ev.target) {
+                    const label = document.createElement('span');
+                    const container = document.createElement('div');
+                    container.setAttribute('id', 'place-label');
+                    label.innerText = name;
+                    container.appendChild(label);
+                    document.body.appendChild(container);
+                    setTimeout(() => {
+                        container.parentElement.removeChild(container);
+                    }, 1500);
+                }
+            };
+            icon.addEventListener('click', clickListener);
+            scene.appendChild(icon);
+        });
+}
