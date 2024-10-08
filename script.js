5fbdc6a985d4d03e5165a486ca11dbdf96bea43d
Fro5tyDog <163802544+Fro5tyDog@users.noreply.github.com>
2024-10-08 14:45:48 +0800

    revert to old tutorial

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const scene = document.querySelector('a-scene');
-    scene.addEventListener('loaded', function () {
-        console.log('A-Frame scene fully initialized');
-        // Place your code here that should run after A-Frame is initialized
-        initializeMyApp();
-    });
-});
-
-function initializeMyApp() {
-// Your custom JavaScript logic here
+window.onload = () => {
     let places = staticLoadPlaces();
     renderPlaces(places);
-}
+};
 
 function staticLoadPlaces() {
    return [
@@ -25,27 +15,25 @@ function staticLoadPlaces() {
    ];
 }
 
-
 function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
- 
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
- 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.15 0.15 0.15');
- 
-        model.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-            console.log("Entity loaded");
-        });
- 
-        scene.appendChild(model);
-    });
- }
+   let scene = document.querySelector('a-scene');
 
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.15 0.15 0.15');
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+           console.log("model loaded");
+       });
+
+       scene.appendChild(model);
+   });
+}
