fdd4484ec1388c76f79b2b054ff94ad4db7ce47f
Fro5tyDog <163802544+Fro5tyDog@users.noreply.github.com>
2024-10-14 13:33:46 +0800

    adding second pokemon

--- script.js
+++ script.js
@@ -15,9 +15,18 @@ function staticLoadPlaces() {
     return [
         {
             name: 'Magnemite',
-            location: {
+            filePath: './assets/magnemite/scene.gltf',
+            location: { //LTA 
                 lat: 1.3087085765187283,
                 lng: 103.85002403454892,
+            }
+        },
+        {
+            name: 'Dragonite',
+            filePath: './assets/dragonite/scene.gltf',
+            location: { //Little India
+                lat: 1.306656407996899,
+                lng: 103.85012141436107,
             }
         },
     ];
@@ -31,12 +40,13 @@ function renderPlaces(places) {
     places.forEach((place) => {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
+        let filePath = place.filePath;
 
         // Create the model only once if it doesn't exist
         if (model === null) {
             model = document.createElement('a-entity');
             model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-            model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+            model.setAttribute('gltf-model', `${filePath}`);
             model.setAttribute('rotation', '0 0 0');
             model.setAttribute('animation-mixer', '');
             model.setAttribute('scale', '0.15 0.15 0.15'); // Initial scale
