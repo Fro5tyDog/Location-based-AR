fa739e809e3edaa88a9568461faeca9a2f6a5c72
Fro5tyDog <163802544+Fro5tyDog@users.noreply.github.com>
2024-10-16 09:02:01 +0800

    remove placeholder text

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function initializeMyApp() {
                     const closestModel = getClosestModel(playerPosition, data);
                     // Update the text with the closest model's name
                     const locationText = document.getElementById('closest-location');
-                    locationText.textContent = `Closest model: ${closestModel.name}`;
+                    locationText.innerHTML = `Closest model: ${closestModel.name}`;
                     console.log(`Closest model is: ${closestModel.name}`);
                 }
             });
