af3c40c34ae11425d3f0a50b010cad38c71a2d2d
Fro5tyDog <163802544+Fro5tyDog@users.noreply.github.com>
2024-10-21 15:34:21 +0800

    add console.dog for latitude

--- script.js
+++ script.js
@@ -122,47 +122,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // runs the calculation for getting the direction which the arrow needs to point
     function runCalculation(event) {
+        if (current.latitude === null || current.longitude === null) {
+            console.error("Current position is not set.");
+            return; // Exit the function early if geolocation hasn't been set
+        }
+    
         var alpha = Math.abs(360 - event.webkitCompassHeading) || event.alpha;
-
         if (alpha == null || Math.abs(alpha - lastAlpha) > 1) {
-        var lat1 = current.latitude * (Math.PI / 180);
-        var lon1 = current.longitude * (Math.PI / 180);
-        var lat2 = target.latitude * (Math.PI / 180);
-        var lon2 = target.longitude * (Math.PI / 180);
-
-        // calculate compass direction
-        var y = Math.sin(lon2 - lon1) * Math.cos(lat2);
-        var x =
-            Math.cos(lat1) * Math.sin(lat2) -
-            Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1);
-        var bearing = Math.atan2(y, x) * (180 / Math.PI);
-
-        direction = (alpha + bearing + 360) % 360;
-        direction = direction.toFixed(0);
-
-        lastAlpha = alpha;
-
-        var R = 6371; // Radius of the earth in km
-        var dLat = lat2 - lat1; // below
-        var dLon = lon2 - lon1;
-        var a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        distance = R * c; // Distance in km
-        distance = distance * 1000; // Convert to meters
-
-
-        var distanceElement = document.getElementById('closest-location');
+            var lat1 = current.latitude * (Math.PI / 180);
+            var lon1 = current.longitude * (Math.PI / 180);
+            var lat2 = target.latitude * (Math.PI / 180);
+            var lon2 = target.longitude * (Math.PI / 180);
+    
+            // calculate compass direction
+            var y = Math.sin(lon2 - lon1) * Math.cos(lat2);
+            var x =
+                Math.cos(lat1) * Math.sin(lat2) -
+                Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1);
+            var bearing = Math.atan2(y, x) * (180 / Math.PI);
+    
+            direction = (alpha + bearing + 360) % 360;
+            direction = direction.toFixed(0);
+    
+            lastAlpha = alpha;
+    
+            var R = 6371; // Radius of the earth in km
+            var dLat = lat2 - lat1; 
+            var dLon = lon2 - lon1;
+            var a =
+                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            distance = R * c; // Distance in km
+            distance = distance * 1000; // Convert to meters
+    
+            var distanceElement = document.getElementById('closest-location');
             if (distance > 20000) {
                 distanceElement.innerHTML = 'Please Select Destination!';
-            } else if (distance <= 15){
+            } else if (distance <= 15) {
                 distanceElement.innerHTML = 'Too close!';
-            }else{
-            // Display the actual distance
+            } else {
                 distanceElement.innerHTML = Math.floor(distance) + "m to destination!";
             }
-        }   
+        }
     };
 
     // starts updating the UI.
